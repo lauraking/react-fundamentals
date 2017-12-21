b00d0e6e9de413a7ea5834c6d0e0a125c04a5eec
Laura Ann Kingsley <lkingsle@thoughtworks.com>
2017-12-21 12:18:55 -0500

    added friends list UI elements

--- app/index.js
+++ app/index.js
@@ -2,15 +2,48 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
 
-class App extends React.Component {
-	render() {
-		return (
-				<div>Hello Squad!</div>
-		)
-	}
+class Users extends React.Component {
+  render() {
+    var friends = this.props.list.filter(function (user) {
+            return user.friend === true;
+          });
+
+    var nonfriends = this.props.list.filter(function (user) {
+            return user.friend !== true;
+          });
+
+    return (
+      <div>
+        <h1>Friends</h1>
+        <ul>
+         {friends.map(function (friend) {
+            return <li key={friend.name}>{friend.name}</li>
+          })
+         }
+        </ul>
+
+        <hr />
+
+        <h1> Non Friends </h1>
+        <ul>
+          {nonfriends.map(function (nonfriend) {
+            return <li key={nonfriend.name}>{nonfriend.name}</li>
+          })
+         }
+        </ul>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
-	<App />,
-	document.getElementById('app')
+  <Users list={[
+    { name: 'Tyler', friend: true },
+    { name: 'Ryan', friend: true },
+    { name: 'Michael', friend: false },
+    { name: 'Mikenzi', friend: false },
+    { name: 'Jessica', friend: true },
+    { name: 'Dan', friend: false } ]}
+  />,
+  document.getElementById('app')
 );
